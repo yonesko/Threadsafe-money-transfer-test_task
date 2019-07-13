7078052c976b69faec77ddd258f77f71c46872b4
Gleb Danichev <glebio@yandex-team.ru>
2019-07-13 19:16:03 +0300

    transferTest

--- src/test/java/glebio/bank/api/TransferControllerTest.java
+++ src/test/java/glebio/bank/api/TransferControllerTest.java
@@ -1,8 +1,16 @@
 package glebio.bank.api;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import glebio.bank.data.Account;
 import glebio.bank.data.Db;
 import glebio.bank.data.Transfer;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -11,17 +19,32 @@ import org.junit.Test;
 public class TransferControllerTest {
 
     @Test
-    public void transferTest() {
+    public void transferTest() throws InterruptedException, ExecutionException {
         TransferController transferController = new TransferController();
+        AccountController accountController = new AccountController();
         Account a = new Account();
         Account b = new Account();
         Db.getInstance().addAccount(a);
         Db.getInstance().addAccount(b);
 
-        Transfer transfer = new Transfer(a.getId(), a.getId(), 341);
-
-        transferController.transfer(transfer);
+        accountController.replenish(a.getId(), 500_000);
 
 
+        System.out.println(a);
+        System.out.println(b);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        List<Future<?>> futures = new LinkedList<>();
+        for (int i = 0; i < 500_000; i++) {
+            futures.add(
+                executorService.submit(() -> transferController.transfer(new Transfer(a.getId(), b.getId(), 1))));
+        }
+
+        for (Future<?> future : futures) {
+            future.get();
+        }
+
+        Assert.assertEquals(a.getCents(), 0);
+        Assert.assertEquals(b.getCents(), 500_000);
     }
 }
