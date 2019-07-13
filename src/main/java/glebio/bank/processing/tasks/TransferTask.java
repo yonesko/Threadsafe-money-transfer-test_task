d80eebc00a977fa7c5281018a4c92fdac9c48c71
Gleb Danichev <glebio@yandex-team.ru>
2019-07-13 17:09:24 +0300

    TransferTask

---
+++ src/main/java/glebio/bank/processing/tasks/TransferTask.java
@@ -0,0 +1,15 @@
+package glebio.bank.processing.tasks;
+
+
+/**
+ * @author Gleb Danichev
+ */
+public class TransferTask implements Runnable {
+
+
+
+    @Override
+    public void run() {
+
+    }
+}
