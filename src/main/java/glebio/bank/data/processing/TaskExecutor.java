523c2f8d6e514629889e47df8dcf7481c27a2b55
Gleb Danichev <glebio@yandex-team.ru>
2019-07-13 17:08:22 +0300

    TaskExecutor

---
+++ src/main/java/glebio/bank/data/processing/TaskExecutor.java
@@ -0,0 +1,17 @@
+package glebio.bank.data.processing;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author Gleb Danichev
+ */
+public class TaskExecutor {
+
+    private final ExecutorService executorService = Executors.newCachedThreadPool();
+
+    public void schedule(Runnable runnable) {
+        executorService.execute(runnable);
+    }
+}
