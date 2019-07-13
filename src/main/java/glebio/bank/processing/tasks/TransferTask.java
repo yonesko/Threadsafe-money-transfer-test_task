21d6eb68634b48ae4ed1db3457426e04a1abfe2a
Gleb Danichev <glebio@yandex-team.ru>
2019-07-13 17:09:52 +0300

    TransferTask

--- src/main/java/glebio/bank/processing/tasks/TransferTask.java
+++ src/main/java/glebio/bank/processing/tasks/TransferTask.java
@@ -1,12 +1,18 @@
 package glebio.bank.processing.tasks;
 
 
+import glebio.bank.data.Transfer;
+
 /**
  * @author Gleb Danichev
  */
 public class TransferTask implements Runnable {
 
+    private final Transfer transfer;
 
+    public TransferTask(Transfer transfer) {
+        this.transfer = transfer;
+    }
 
     @Override
     public void run() {
