6c81c27d100b725e365ffd0c46b02692d04312ce
Gleb Danichev <glebio@yandex-team.ru>
2019-07-13 16:47:17 +0300

    hashCode

--- src/main/java/glebio/bank/data/Account.java
+++ src/main/java/glebio/bank/data/Account.java
@@ -2,17 +2,20 @@ package glebio.bank.data;
 
 import java.util.UUID;
 
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
+
 /**
  * @author Gleb Danichev
  */
 public class Account {
 
-    private final UUID uuid = UUID.randomUUID();
+    private final UUID id = UUID.randomUUID();
 
     private long cents;
 
-    public UUID getUuid() {
-        return uuid;
+    public UUID getId() {
+        return id;
     }
 
     public long getCents() {
@@ -21,5 +24,26 @@ public class Account {
 
     public void setCents(long cents) {
         this.cents = cents;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return cents == account.cents && Objects.equal(id, account.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id, cents);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+            .add("id", id)
+            .add("cents", cents)
+            .toString();
     }
 }
