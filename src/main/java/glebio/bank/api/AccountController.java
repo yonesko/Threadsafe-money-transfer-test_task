658e306a2c2a121cd0cee2ea3df6836b9ce94afa
Gleb Danichev <glebio@yandex-team.ru>
2019-07-13 17:17:29 +0300

    addAccount

--- src/main/java/glebio/bank/api/AccountController.java
+++ src/main/java/glebio/bank/api/AccountController.java
@@ -1,9 +1,19 @@
 package glebio.bank.api;
 
 
+import java.util.UUID;
+
+import glebio.bank.data.Account;
+import glebio.bank.data.Db;
+
 /**
  * @author Gleb Danichev
  */
 public class AccountController {
 
+    public UUID addAccount() {
+        Account account = new Account();
+        Db.getInstance().addAccount(account);
+        return account.getId();
+    }
 }
--- src/main/java/glebio/bank/data/Db.java
+++ src/main/java/glebio/bank/data/Db.java
@@ -19,4 +19,8 @@ public class Db {
 
     private Db() {
     }
+
+    public void addAccount(Account account) {
+        accounts.putIfAbsent(account.getId(), account);
+    }
 }
