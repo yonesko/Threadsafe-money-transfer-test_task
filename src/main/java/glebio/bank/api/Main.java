f6a8ff716303d0324f657bc7983f6a1100b4cfa2
Gleb Danichev <glebio@yandex-team.ru>
2019-07-14 18:59:16 +0300

    Start Server

--- src/main/java/glebio/bank/api/Main.java
+++ src/main/java/glebio/bank/api/Main.java
@@ -4,7 +4,6 @@ import javax.ws.rs.core.UriBuilder;
 
 import org.eclipse.jetty.server.Server;
 import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
 
 /**
  * @author Gleb Danichev
@@ -13,10 +12,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Server server = JettyHttpContainerFactory.createServer(
-            UriBuilder.fromUri("http://localhost/").port(9998).build(),
-            new ResourceConfig(AccountController.class)
+            UriBuilder.fromUri("http://localhost/").port(9998).build()
         );
-        server.start();
-        server.join();
     }
 }
