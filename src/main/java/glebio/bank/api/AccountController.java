6837935f154e45aa1b10a36c7eaf16546968d601
Gleb Danichev <glebio@yandex-team.ru>
2019-07-14 21:40:23 +0300

    createServer

--- src/main/java/glebio/bank/api/AccountController.java
+++ src/main/java/glebio/bank/api/AccountController.java
@@ -4,6 +4,10 @@ package glebio.bank.api;
 import java.util.UUID;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import glebio.bank.data.Account;
 import glebio.bank.data.Db;
@@ -11,6 +15,7 @@ import glebio.bank.data.Db;
 /**
  * @author Gleb Danichev
  */
+@Path("/account")
 public class AccountController {
 
     public UUID addAccount() {
@@ -20,7 +25,9 @@ public class AccountController {
     }
 
     @GET
-    public Account getAccount(UUID accountId) {
+    @Path("/{accountId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Account getAccount(@PathParam("accountId") UUID accountId) {
         return Db.getInstance().getAccount(accountId).orElseThrow();
     }
 
--- src/main/java/glebio/bank/api/Main.java
+++ src/main/java/glebio/bank/api/Main.java
@@ -1,18 +1,20 @@
 package glebio.bank.api;
 
+import java.net.URI;
+
 import javax.ws.rs.core.UriBuilder;
 
-import org.eclipse.jetty.server.Server;
 import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
 
 /**
  * @author Gleb Danichev
  */
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        Server server = JettyHttpContainerFactory.createServer(
-            UriBuilder.fromUri("http://localhost/").port(9998).build()
-        );
+    public static void main(String[] args) {
+        URI baseUri = UriBuilder.fromUri("http://localhost/").port(9998).build();
+        ResourceConfig config = new ResourceConfig(AccountController.class);
+        JettyHttpContainerFactory.createServer(baseUri, config);
     }
 }
