edc5577039d4bd066875df09022a0d586280ea9e
Gleb Danichev <glebio@yandex-team.ru>
2019-07-15 14:32:36 +0300

    TransferController

--- example.bash
+++ example.bash
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
-myAccountId=$(curl  -X POST  "http://localhost:9998/account")
-wifeAccountId=$(curl  -X POST  "http://localhost:9998/account")
+myAccountId=$(curl -s -X POST  "http://localhost:9998/account")
+wifeAccountId=$(curl -s -X POST  "http://localhost:9998/account")
 
 echo "Created accounts in Glebio Bank"
 
-curl "http://localhost:9998/account/$myAccountId"; echo
-curl "http://localhost:9998/account/$wifeAccountId"; echo
+curl -s "http://localhost:9998/account/$myAccountId"; echo
+curl -s "http://localhost:9998/account/$wifeAccountId"; echo
 
-curl -X POST "http://localhost:9998/account/$myAccountId/replenish?cents=500000"
+curl -s -X POST "http://localhost:9998/account/$myAccountId/replenish?cents=500000"
 
 echo 'Add $5000 to my account'
 
-curl "http://localhost:9998/account/$myAccountId"; echo
+curl -s "http://localhost:9998/account/$myAccountId"; echo
 
 echo 'Transfer $1000 to my wife'
 
-curl -X POST "http://localhost:9998/transfer?from=$myAccountId&to=$wifeAccountId&cents=100000"; echo
+curl -s -X POST "http://localhost:9998/transfer?from=$myAccountId&to=$wifeAccountId&cents=100000"; echo
 
-echo "My account: $(curl "http://localhost:9998/account/$myAccountId")"
+echo "My account: $(curl -s "http://localhost:9998/account/$myAccountId")"
 
