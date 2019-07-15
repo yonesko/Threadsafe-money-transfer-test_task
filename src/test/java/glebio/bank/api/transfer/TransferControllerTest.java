00310bf564e486f63654ae3e532b1aa2642aa6b1
Gleb Danichev <glebio@yandex-team.ru>
2019-07-15 16:50:46 +0300

    TransferControllerTest

--- src/test/java/glebio/bank/api/transfer/TransferControllerTest.java
+++ src/test/java/glebio/bank/api/transfer/TransferControllerTest.java
@@ -1,26 +1,29 @@
 package glebio.bank.api.transfer;
 
-import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
 
-import glebio.bank.api.account.AccountController;
-import glebio.bank.api.jersey.UUIDMessageBodyWriter;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.test.JerseyTest;
-import org.glassfish.jersey.test.TestProperties;
+import glebio.bank.api.AbstractControllerIntegrationTest;
+import glebio.bank.data.model.Account;
+import org.junit.Assert;
 import org.junit.Test;
 
-public class TransferControllerTest extends JerseyTest {
-
-    @Override
-    protected Application configure() {
-        enable(TestProperties.LOG_TRAFFIC);
-        enable(TestProperties.DUMP_ENTITY);
-        return new ResourceConfig(AccountController.class, UUIDMessageBodyWriter.class);
-    }
+public class TransferControllerTest extends AbstractControllerIntegrationTest {
 
     @Test
     public void transferTest() {
+        Account tomAccount = createAccount();
+        Account zendayaAccount = createAccount();
 
+        Response replenishResponse = replenish(tomAccount.getId(), 5000 * 100);
+        Assert.assertEquals(Response.Status.OK.getStatusCode(), replenishResponse.getStatus());
+
+        Response transferResponce = target("transfer")
+            .queryParam("from", tomAccount.getId().toString())
+            .queryParam("to", tomAccount.getId().toString())
+            .queryParam("cents", 1234 * 100)
+            .request().method("POST");
+
+        Assert.assertEquals(Response.Status.OK.getStatusCode(), transferResponce.getStatus());
     }
 
     @Test
