9100a53a5a05d720f970d4310357a5cc786174c4
Gleb Danichev <glebio@yandex-team.ru>
2019-07-15 14:36:19 +0300

    TransferController

--- example.bash
+++ example.bash
@@ -4,18 +4,20 @@ wifeAccountId=$(curl -s -X POST  "http://localhost:9998/account")
 
 echo "Created accounts in Glebio Bank"
 
-curl -s "http://localhost:9998/account/$myAccountId"; echo
-curl -s "http://localhost:9998/account/$wifeAccountId"; echo
+echo "My account: $(curl -s "http://localhost:9998/account/$myAccountId")"
+echo "Wife's account: $(curl -s "http://localhost:9998/account/$wifeAccountId")"
 
 curl -s -X POST "http://localhost:9998/account/$myAccountId/replenish?cents=500000"
 
 echo 'Add $5000 to my account'
 
-curl -s "http://localhost:9998/account/$myAccountId"; echo
+echo "My account: $(curl -s "http://localhost:9998/account/$myAccountId")"
+echo "Wife's account: $(curl -s "http://localhost:9998/account/$wifeAccountId")"
 
 echo 'Transfer $1000 to my wife'
 
 curl -s -X POST "http://localhost:9998/transfer?from=$myAccountId&to=$wifeAccountId&cents=100000"; echo
 
 echo "My account: $(curl -s "http://localhost:9998/account/$myAccountId")"
+echo "Wife's account: $(curl -s "http://localhost:9998/account/$wifeAccountId")"
 
