041fc90a326c872e43eb62563b59b7d393e7ccc3
Gleb Danichev <glebio@yandex-team.ru>
2019-07-15 13:31:39 +0300

    configureServer

--- src/main/java/glebio/bank/api/Main.java
+++ src/main/java/glebio/bank/api/Main.java
@@ -2,6 +2,8 @@ package glebio.bank.api;
 
 import javax.ws.rs.core.UriBuilder;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.Slf4jRequestLog;
 import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -10,10 +12,17 @@ import org.glassfish.jersey.server.ResourceConfig;
  */
 public class Main {
 
-    public static void main(String[] args) {
-        JettyHttpContainerFactory.createServer(
+    public static void main(String[] args) throws Exception {
+        Server server = JettyHttpContainerFactory.createServer(
             UriBuilder.fromUri("http://localhost/").port(9998).build(),
             new ResourceConfig(AccountController.class, UUIDMessageBodyWriter.class)
         );
+        configureServer(server);
+    }
+
+    private static void configureServer(Server server) throws Exception {
+        Slf4jRequestLog requestLog = new Slf4jRequestLog();
+        requestLog.start();
+        server.setRequestLog(requestLog);
     }
 }
