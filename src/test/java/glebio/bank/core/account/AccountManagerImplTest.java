0883ebb6d00f31d9900961965f4b840719c0067b
Gleb Danichev <glebio@yandex-team.ru>
2019-07-15 14:00:56 +0300

    replenishRaceConditionTest

---
+++ src/test/java/glebio/bank/core/account/AccountManagerImplTest.java
@@ -0,0 +1,35 @@
+package glebio.bank.core.account;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import glebio.bank.data.Account;
+import glebio.bank.data.Db;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AccountManagerImplTest {
+
+    @Test
+    public void replenishRaceConditionTest() throws ExecutionException, InterruptedException {
+        AccountManager accountManager = new AccountManagerImpl();
+        Account a = new Account();
+        Db.getInstance().addAccount(a);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        List<Future<?>> futures = new LinkedList<>();
+        for (int i = 0; i < 500_000; i++) {
+            futures.add(
+                executorService.submit(() -> accountManager.replenish(a.getId(), 1)));
+        }
+
+        for (Future<?> future : futures) {
+            future.get();
+        }
+        Assert.assertEquals(a.getCents(), 500_000);
+    }
+}
